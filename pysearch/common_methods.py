05772f96e57afda62191beef7ee624e28ce4710a
Sourabh Kumbhar <55979623+Sourabh2001-dec@users.noreply.github.com>
2023-04-21 12:27:45 +0530

    method changed

--- pysearch/common_methods.py
+++ pysearch/common_methods.py
@@ -3,28 +3,30 @@ import pickle
 import json
 from functools import lru_cache
 
-## global variables
-pyfolder = os.path.dirname( os.path.abspath(__file__))
-parent_dir =  os.path.dirname(pyfolder)
-global_info_path = os.path.join(parent_dir,"global_info.json")
+# global variables
+pyfolder = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(pyfolder)
+global_info_path = os.path.join(parent_dir, "global_info.json")
 
 
-def save_pickle_obj(obj,path):
+def save_pickle_obj(obj, path):
     with open(path, 'wb') as f:
         pickle.dump(obj, f)
 
+
 @lru_cache
 def load_pickle_obj(path):
-    with open( path, 'rb') as f:
-        return  pickle.load(f)
-    
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+
 @lru_cache
-def load_json_file(path = global_info_path):
+def load_json_file(path=global_info_path):
     with open(path, 'r') as f:
         return json.load(f)
 
 
-def load_field_from_json(path = global_info_path ,field= "ignore_file_types"):
+def load_field_from_json(path=global_info_path, field="ignore_file_types"):
     content = load_json_file(path)
     return content[field]
 
@@ -38,35 +40,27 @@ def make_file_content(row):
 
     Generate file content for the given row of metadata.
     """
-    file_path, is_directory, file_type, created_time,file_name,size = row
-    
-    # Generate a human-readable size string
-    if size < 1024:
-        size_str = str(size) + " bytes"
-    elif size < 1048576:
-        size_str = str(round(size / 1024, 2)) + " KB"
-    else:
-        size_str = str(round(size / 1048576, 2)) + " MB"
-        
-    is_folder = "folder" if is_directory else "file"
-    
-    # Generate the file content string
-    content = f"{is_folder} named {file_name} type of {is_folder} {file_type}  with a size of {size_str}. " \
-            f"created on {created_time} " \
-            f"{is_folder} location {file_path}."       
+    file_path, is_directory, file_type, created_time, file_name, size = row
 
-    return content
+    # # Generate a human-readable size string
+    # if size < 1024:
+    #     size_str = str(size) + " bytes"
+    # elif size < 1048576:
+    #     size_str = str(round(size / 1024, 2)) + " KB"
+    # else:
+    #     size_str = str(round(size / 1048576, 2)) + " MB"
 
+    # is_folder = "folder" if is_directory else "file"
 
+    # # Generate the file content string
+    # content = f"{is_folder} named {file_name} type of {is_folder} {file_type}  with a size of {size_str}. " \
+    #         f"created on {created_time} " \
+    #         f"{is_folder} location {file_path}."
 
-
-
-       
+    return "file name " + file_name
 
 
 if __name__ == "__main__":
     # row = ("mk.py","pdf",230,"Tue Mar  7 10:01:25 2023","Tue Mar  7 10:01:25 2023","Wed Oct 21 22:55:37 2020",r"C:\Users\shree\OneDrive\Documents\21142_DEL_Comparator_expt.no. 05.pdf")
     # print(make_file_content(row))
     print(parent_dir)
-
-     
